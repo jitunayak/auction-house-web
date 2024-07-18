4111c6144054855dce5c2182c4dad15d1a0f3c13
jitunayak <jitunayak715@gmail.com>
2024-07-18 13:07:47 +0530

    resized logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import logo from "@/assets/logo_black.png";
 import { Link } from "@tanstack/react-router";
 import { Button } from "./ui/button";
 
@@ -5,7 +6,11 @@ function Navbar() {
   return (
     <header className="fixed top-0 right-0 left-0 w-full flex items-center justify-between  px-4 py-2 z-[100] backdrop-blur-lg">
       <Link to="/">
-        <div className="text-xl font-semibold">Auction House</div>
+        <img
+          src={logo}
+          alt="logo"
+          className=" aspect-auto h-[2.6rem] rounded"
+        />
       </Link>
       <nav className="absolute left-[50%] transform translate-x-[-50%] hidden md:block">
         <ul className="list-none flex gap-4 items-center ">
